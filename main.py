dbadb360dc12609474183efa03c1056634198562
lowkhoo <224758456+105214178@users.noreply.github.com>
2026-04-27 20:50:46 +1000

    Add LSTM model with MAE and RMSE evaluation

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from preprocessing import build_dataset
+from models.lstm import run_lstm
+
+
 
 FILEPATH = 'data/Scats Data October 2006.xls'
 
 X_train, X_test, y_train, y_test, scalers, coords, agg = build_dataset(FILEPATH)
+
+# -- Run LSTM model --
+predictions_scaled, y_test = run_lstm(X_train, X_test, y_train, y_test, scalers)
---
+++ models/lstm.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras import Input
+from tensorflow.keras.callbacks import EarlyStopping
+
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+def run_lstm(X_train, X_test, y_train, y_test, scalers):
+
+    model = Sequential()
+    model.add(Input(shape=(12, 1)))
+    model.add(LSTM(50))
+    model.add(Dense(1))
+
+    model.compile(optimizer='adam', loss='mse')
+
+    early_stop = EarlyStopping(
+        monitor='val_loss',
+        patience=3,
+        restore_best_weights=True
+    )
+
+    model.fit(
+        X_train,
+        y_train,
+        epochs=30,
+        batch_size=32,
+        validation_split=0.2,
+        callbacks=[early_stop]
+    )
+
+    predictions_scaled = model.predict(X_test)
+
+    mae = mean_absolute_error(y_test, predictions_scaled)
+    rmse = np.sqrt(mean_squared_error(y_test, predictions_scaled))
+
+    print("LSTM MAE:", mae)
+    print("LSTM RMSE:", rmse)
+
+    return predictions_scaled, y_test
